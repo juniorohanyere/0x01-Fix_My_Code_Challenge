8fddef9c8db7a10eb3cf6e7be73d2104341fcda2
Twin J <junohanyere@gmail.com>
2023-12-10 19:38:35 +0100

    print square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,14 +6,12 @@
 class Square():
     """prints a square
     """
-
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
         """initialise self
         """
-
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -24,20 +22,17 @@ class Square():
     def perimeter_of_my_square(self):
         """return the perimeter of a square
         """
-
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """return string representation of the width and height of a square
         """
-
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
     """wrapper line
     """
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
