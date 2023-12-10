347ceee2efced785e2124e81268cad6ab29567fb
Twin J <junohanyere@gmail.com>
2023-12-10 18:58:47 +0100

    fixed: route for the status of the API

--- status_server/api/v1/views/index.py
+++ status_server/api/v1/views/index.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify
+from flask import *
 
 from api.v1.views import app_views
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
     """
-    return jsonify({"status": "OK"})
+    return make_response(jsonify({"status": "OK"}), 200)
